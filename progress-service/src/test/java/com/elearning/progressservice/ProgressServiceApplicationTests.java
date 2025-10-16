20f66845aa9c8d022deaf85d9ed99ef74ad17100
nguyengiangminh <nguyengiangminh99@gmail.com>
2025-10-16 10:05:09 +0700

    create project

---
+++ progress-service/src/test/java/com/elearning/progressservice/ProgressServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elearning.progressservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
